521b908b2ab5cf84b14bddbd8b7b4adc56512970
Sarah Singh <sarah.h.singh@gmail.com>
2022-05-11 21:54:44 -0700

    implemented suggested changes to bitset::set()

--- tests/bitset.test.cpp
+++ tests/bitset.test.cpp
@@ -2,198 +2,174 @@
 #include <cstdint>
 #include <libxbitset/bitset.hpp>
 
-namespace embed {
+namespace xstd {
 boost::ut::suite bitset_test = [](){
     using namespace boost::ut;
 
-    //TEST SET() FOR INITIAL VALUE OF 1
-    "bitset::set()_set_no_change_1"_test = [](){
-        xstd::bitset<int> test_set(1); 
+    "bitset::set() set no change 1"_test = [](){
+        xstd::bitset test_set(1); 
         //maintain intial value for odd number using set(0,true)
         expect(that % 0x0000'0001 == test_set.set(0,true)); 
         expect(that % 0x0000'0001 == test_set.set(0,true));
     };
 
-    "bitset::set()_increment_position_1"_test = [](){
-        xstd::bitset<int> test_set(1);
-        //incrementing position with same value
+    "bitset::set() increment position 1"_test = [](){
+        xstd::bitset test_set(1); 
         expect(that % 0x0000'0001 == test_set.set(0,true)); 
-        expect(that % 0x0000'0003 == test_set.set(1,true));
-        expect(that % 0x0000'0007 == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'0003 == test_set.set(1));
+        expect(that % 0x0000'0007 == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_value_1"_test = [](){
-        xstd::bitset<int> test_set(1);
-        //incrementing value with same position
+    "bitset::set() increment value 1"_test = [](){
+        xstd::bitset test_set(1);
         expect(that % 0x0000'0000 == test_set.set(0,false));
         expect(that % 0x0000'0001 == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 2
-    "bitset::set()_set_no_change_2"_test = [](){
-        xstd::bitset<int> test_set(2); 
+    "bitset::set() set no change 2"_test = [](){
+        xstd::bitset test_set(2);
         //maintain initial value for even number using set(0,false)
         expect(that % 0x0000'0002 == test_set.set(0,false));
         expect(that % 0x0000'0002 == test_set.set(0,false));
     };  
 
-    "bitset::set()_increment_position_2"_test = [](){
-        xstd::bitset<int> test_set(2);
-        //incrementing position with same value
+    "bitset::set() increment position 2"_test = [](){
+        xstd::bitset test_set(2);
         expect(that % 0x0000'0003 == test_set.set(0,true));
-        expect(that % 0x0000'0003 == test_set.set(1,true));
-        expect(that % 0x0000'0007 == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'0003 == test_set.set(1));
+        expect(that % 0x0000'0007 == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_2"_test = [](){
-        xstd::bitset<int> test_set(2);
-        //incrementing value with same position
+    "bitset::set() increment val 2"_test = [](){
+        xstd::bitset test_set(2);
         expect(that % 0x0000'0002 == test_set.set(0,false));
         expect(that % 0x0000'0003 == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 3
-    "bitset::set()_set_no_change_3"_test = [](){
-        xstd::bitset<int> test_set(3); 
+    "bitset::set() set no change 3"_test = [](){
+        xstd::bitset test_set(3); 
         //maintain intial value for odd number using set(0,true)
         expect(that % 0x0000'0003 == test_set.set(0,true));
         expect(that % 0x0000'0003 == test_set.set(0,true));
     };  
 
-     "bitset::set()_increment_position_3"_test = [](){
-        xstd::bitset<int> test_set(3);
-        //incrementing position with same value
+     "bitset::set() increment position 3"_test = [](){
+        xstd::bitset test_set(3);
         expect(that % 0x0000'0003 == test_set.set(0,true));
-        expect(that % 0x0000'0003 == test_set.set(1,true));
-        expect(that % 0x0000'0007 == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'0003 == test_set.set(1));
+        expect(that % 0x0000'0007 == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_3"_test = [](){
-        xstd::bitset<int> test_set(3);
-        //incrementing value with same position
+    "bitset::set() increment val 3"_test = [](){
+        xstd::bitset test_set(3);
         expect(that % 0x0000'0002 == test_set.set(0,false));
         expect(that % 0x0000'0003 == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 4
-    "bitset::set()_set_no_change_4"_test = [](){
-        xstd::bitset<int> test_set(4); 
+    "bitset::set() set no change 4"_test = [](){
+        xstd::bitset test_set(4); 
         //maintain initial value for even number using set(0,false)
         expect(that % 0x0000'0004 == test_set.set(0,false));
         expect(that % 0x0000'0004 == test_set.set(0,false));
     };  
 
-    "bitset::set()_increment_position_4"_test = [](){
-        xstd::bitset<int> test_set(4);
-        //incrementing position with same value
+    "bitset::set() increment position 4"_test = [](){
+        xstd::bitset test_set(4);
         expect(that % 0x0000'0005 == test_set.set(0,true));
-        expect(that % 0x0000'0007 == test_set.set(1,true));
-        expect(that % 0x0000'0007 == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'0007 == test_set.set(1));
+        expect(that % 0x0000'0007 == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_4"_test = [](){
-        xstd::bitset<int> test_set(4);
-        //incrementing value with same position
+    "bitset::set() increment val 4"_test = [](){
+        xstd::bitset test_set(4);
         expect(that % 0x0000'0004 == test_set.set(0,false));
         expect(that % 0x0000'0005 == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 5
-    "bitset::set()_set_no_change_5"_test = [](){
-        xstd::bitset<int> test_set(5); 
+    "bitset::set() set no change 5"_test = [](){
+        xstd::bitset test_set(5); 
         //maintain intial value for odd number using set(0,true)
         expect(that % 0x0000'0005 == test_set.set(0,true));
         expect(that % 0x0000'0005 == test_set.set(0,true));
     };  
 
-     "bitset::set()_increment_position_5"_test = [](){
-        xstd::bitset<int> test_set(5);
-        //incrementing position with same value
+     "bitset::set() increment position 5"_test = [](){
+        xstd::bitset test_set(5);
         expect(that % 0x0000'0005 == test_set.set(0,true));
-        expect(that % 0x0000'0007 == test_set.set(1,true));
-        expect(that % 0x0000'0007 == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'0007 == test_set.set(1));
+        expect(that % 0x0000'0007 == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_5"_test = [](){
-        xstd::bitset<int> test_set(5);
-        //incrementing value with same position
+    "bitset::set() increment val 5"_test = [](){
+        xstd::bitset test_set(5);
         expect(that % 0x0000'0004 == test_set.set(0,false));
         expect(that % 0x0000'0005 == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 10
-    "bitset::set()_set_no_change_10"_test = [](){
-        xstd::bitset<int> test_set(0xA); 
+    "bitset::set() set no change 10"_test = [](){
+        xstd::bitset test_set(0xA); 
         //maintain initial value for even number using set(0,false)
         expect(that % 0x0000'000A == test_set.set(0,false));
         expect(that % 0x0000'000A == test_set.set(0,false));
     };  
 
-    "bitset::set()_increment_position_10"_test = [](){
-        xstd::bitset<int> test_set(0xA);
-        //incrementing position with same value
+    "bitset::set() increment position 10"_test = [](){
+        xstd::bitset test_set(0xA);
         expect(that % 0x0000'000B == test_set.set(0,true));
-        expect(that % 0x0000'000B == test_set.set(1,true));
-        expect(that % 0x0000'000F == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'000B == test_set.set(1));
+        expect(that % 0x0000'000F == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_10"_test = [](){
-        xstd::bitset<int> test_set(0xA);
-        //incrementing value with same position
+    "bitset::set() increment val 10"_test = [](){
+        xstd::bitset test_set(0xA);
         expect(that % 0x0000'000A == test_set.set(0,false));
         expect(that % 0x0000'000B == test_set.set(0,true));
     };
 
-    //TEST SET() FOR INITIAL VALUE OF 11
-    "bitset::set()_set_no_change_11"_test = [](){
-        xstd::bitset<int> test_set(0xB); 
+    "bitset::set() set no change 11"_test = [](){
+        xstd::bitset test_set(0xB); 
         //maintain intial value for odd number using set(0,true)
         expect(that % 0x0000'000B == test_set.set(0,true));
         expect(that % 0x0000'000B == test_set.set(0,true));
     };  
 
-     "bitset::set()_increment_position_11"_test = [](){
-        xstd::bitset<int> test_set(0xB);
-        //incrementing position with same value
+     "bitset::set() increment position 11"_test = [](){
+        xstd::bitset test_set(0xB);
         expect(that % 0x0000'000B == test_set.set(0,true));
-        expect(that % 0x0000'000B == test_set.set(1,true));
-        expect(that % 0x0000'000F == test_set.set(2,true));
-        expect(that % 0x0000'000F == test_set.set(3,true));
+        expect(that % 0x0000'000B == test_set.set(1));
+        expect(that % 0x0000'000F == test_set.set(2));
+        expect(that % 0x0000'000F == test_set.set(3));
     };
 
-    "bitset::set()_increment_val_11"_test = [](){
-        xstd::bitset<int> test_set(0xB);
-        //incrementing value with same position
+    "bitset::set() increment val 11"_test = [](){
+        xstd::bitset test_set(0xB);
         expect(that % 0x0000'000A == test_set.set(0,false));
         expect(that % 0x0000'000B == test_set.set(0,true));
     };
-
-    //TEST SET() FOR UPPER HALF 
-    "bitset::set()_set_no_change_UH1"_test = [](){
-        xstd::bitset<int> test_set(0x1'FFFF); 
+ 
+    "bitset::set() set no change UH1"_test = [](){
+        xstd::bitset test_set(0x1'FFFF); 
         //maintain intial value for odd number using set(0,true)
         expect(that % 0x0001'FFFF == test_set.set(16,true));
         expect(that % 0x0001'FFFF == test_set.set(16,true));
     };  
 
-     "bitset::set()_increment_position_UH1"_test = [](){
-        xstd::bitset<int> test_set(0x1'FFFF);
-        //incrementing position with same value
+     "bitset::set() increment position UH1"_test = [](){
+        xstd::bitset test_set(0x1'FFFF);
         expect(that % 0x0001'FFFF == test_set.set(16,true));
-        expect(that % 0x0003'FFFF == test_set.set(17,true));
-        expect(that % 0x0007'FFFF == test_set.set(18,true));
-        expect(that % 0x000F'FFFF == test_set.set(19,true));
+        expect(that % 0x0003'FFFF == test_set.set(17));
+        expect(that % 0x0007'FFFF == test_set.set(18));
+        expect(that % 0x000F'FFFF == test_set.set(19));
     };
 
-    "bitset::set()_increment_val_UH1"_test = [](){
-        xstd::bitset<int> test_set(0x1'FFFF);
-        //incrementing value with same position
+    "bitset::set() increment val UH1"_test = [](){
+        xstd::bitset test_set(0x1'FFFF);
         expect(that % 0x0000'FFFF == test_set.set(16,false));
         expect(that % 0x0001'FFFF == test_set.set(16,true));
     };
